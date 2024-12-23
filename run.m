60b104fc1a120f49ea09c54a8fb1ec7813bdbfaa
rnelson26 <ranelson526@gmail.com>
2024-12-23 16:29:55 -0600

    initial attempt to modify code

--- run.m
+++ run.m
@@ -13,42 +13,84 @@
 %
 %  Date:
 %   4/16/2022
+
+%%%%% Updates 12/23/2023 Lincolnshire, IL by Becca Nelson:
+%%% began modification of code for cross-boundary spillover project 
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Run a suite of simulations
 function [] = run()
-global network
+    global network
 
+    % Set directory where .csv files are located
+    network_dir = '/Users/Becca/Desktop/Harrison Lab/crossboundary_spillover';  % Directory with the .csv files
 
-    % if no networks were specified, use all of them
-    if nargin < 1
-        networks = 1:2%1:1:1200;
-    end
+    % Get a list of .csv files in the directory
+    network_files = dir(fullfile(network_dir, '*.csv'));
 
-    % network range 
-    for network_index = 1:length(networks)
-        network = networks(network_index);
+    % Define the columns you want to work with
+    columns_to_keep = {'ASER', 'VIVI', 'CESO', 'MEIN', 'PHAQ', 'ANAR', 'AMME', 'ERCI', 'mustard', 'yellow_aster', 'dandelion', 'MEPO', 'SEVU'};
+
+    % Loop through each CSV file and process
+    for i = 1:length(network_files)
+        % Load the current network CSV file
+        network_file = network_files(i).name;
+        full_file_path = fullfile(network_dir, network_file);
         
-        % run for mortality situation 3 defined below:
-        % 1) animals high mortality
-        % 2) plants high mortality
-        % 3) animals and plants low mortality
-        % 4) animals and plants high mortality
-            death_case = 3;
-                
-                file_name = [network, death_case];
+        % Read the CSV file into a table
+        data = readtable(full_file_path);
 
-                    %%Run simulation 
-                    [Alpha, P, A] = run_inv_PC(file_name);
+        % Create a list of columns to keep (if they exist)
+        existing_columns = intersect(columns_to_keep, data.Properties.VariableNames);  % Check which columns exist
 
-                    %%Write data to a csv file
-                    plant_data = [full(P{1}), full(P{2})];
-                    animal_data = [full(A{1}), full(A{2})];
-                    alpha_data = [full(Alpha{1}), full(Alpha{2})];                
+        % 1) Create CSV with only Non-serpentine ('columns to keep') (data_NS)
+        data_NS = data(:, existing_columns);  % Keep only the NS columns that exist
+        writetable(data_NS, fullfile(network_dir, sprintf('data_NS_%s', network_file)));
 
-                    writematrix(plant_data, sprintf('data/P_n%04d_m%d.csv', file_name));
-                    writematrix(animal_data, sprintf('data/A_n%04d_m%d.csv', file_name));
-                    writematrix(alpha_data, sprintf('data/Alpha_n%04d_m%d.csv', file_name));
-    
+        % 2) Create CSV with all columns except ASER, VIVI, CESO (if they exist) (data_S)
+        columns_to_remove = setdiff(data.Properties.VariableNames, existing_columns);  % Remove NS columns if they exist
+        data_S = data(:, columns_to_remove);  % Keep only serpentine columns
+        writetable(data_S, fullfile(network_dir, sprintf('data_S_%s', network_file)));
+
+        % 3) Run simulations on three versions of the data (original, data_NS, data_S)
+        
+        % a) Original full data (network_data)
+        network_data = data;  % Use the original data structure
+        run_simulation(network_data, network_file, 'Original');
+
+        % b) Non-serpentine data (data_NS)
+        run_simulation(data_NS, network_file, 'Non-serpentine');
+
+        % c) Serpentine data (data_S)
+        run_simulation(data_S, network_file, 'Serpentine');
     end
-end               
+end
+
+function [] = run_simulation(data, network_file, data_type)
+    global network
+
+    % Run the simulation on the provided data (original, data_NS, or data_S)
+    network = data;  % If needed, adjust how you use the data in your simulation
+
+    % Run for mortality situation 3 (low mortality for animals and plants)
+    death_case = 3;
+
+    % Construct a file name for the current network and death case
+    file_name = sprintf('%s_%s_m%d', network_file(1:end-4), data_type, death_case);  % Append the data type to the filename
+
+    %% Run simulation 
+    [Alpha, P, A] = run_inv_PC(data, file_name);
+
+    %% Write data to a csv file
+    plant_data = [full(P{1}), full(P{2})];
+    animal_data = [full(A{1}), full(A{2})];
+    alpha_data = [full(Alpha{1}), full(Alpha{2})];                
+
+    % Write output data to csv files
+    writematrix(plant_data, sprintf('data/P_%s.csv', file_name));
+    writematrix(animal_data, sprintf('data/A_%s.csv', file_name));
+    writematrix(alpha_data, sprintf('data/Alpha_%s.csv', file_name));
+end
+
+
--- run_inv_PC.m
+++ run_inv_PC.m
@@ -1,39 +1,35 @@
-function [Alpha, P, A] = run_inv_PC(file_name)
+%% updated to be compatible with cross-boundary spillover data structure 
 
-% file_name = [network, death_case, link_case, inv_type];
-% network = 1:10
-% death_case = 1:4
-% link_case = 1:3 - run for whether alien links randomly, to generalists,
-% or to specialists
-% inv_type = 1:8 - run for eacy type of invader
+function [Alpha, P, A] = run_inv_PC(data, file_name)
+    % Assumes data is a table or matrix passed from the run function
+    % file_name: string to identify the current network and data type being used
 
-global J_pattern network_data 
+    global J_pattern network_data
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Load Data %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % Check if the data is a table, and convert non-numeric columns to numeric (if necessary)
+    if istable(data)
+        % Remove non-numeric columns from the table
+        numeric_data = data(:, varfun(@isnumeric, data, 'OutputFormat', 'uniform'));
+        
+        % Convert the numeric table to a matrix
+        network_data = table2array(numeric_data);  
+    else
+        % If the data is already a matrix (numeric data), use as is
+        network_data = data;
+    end
 
-% load data
-load('1200m.mat')
-network =file_name(1);
-network_data = cell2mat(m1200(network));
+    % Seed to control random generated numbers
+    seed = 0;
+    rand('seed', seed);  % Control randomness with a fixed seed
 
-% seed to control random generated numbers
-seed = 0;
-rand('seed', seed + network);
+    % Reformat network data by ascending degree (sorting nodes)
+    [sum_a, index_a] = sort(sum(network_data));  % Sorting by node degree
+    [sum_p, index_p] = sort(sum(network_data, 2));  % Sorting by node degree (in reverse)
+    network_data = network_data(index_p, index_a);  % Apply the sorted order to the matrix
 
-% reformat network data by ascending degree
-[sum_a, index_a] = sort(sum(network_data));
-[sum_p, index_p] = sort(sum(network_data, 2));
-network_data = network_data(index_p, index_a); %%why??
+    % Perform Invasion setup (can be adjusted based on your needs)
+    J_pattern = jacobian_pattern(network_data);  % Assuming this function calculates a jacobian matrix
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%% Perform Invasion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% set up invasion
-%[network_data] = invasion_setup(network_data, file_name);
-
-% create jacobian matrix
-J_pattern = jacobian_pattern(network_data);
-
-% start simulation  
-[Alpha, P, A] = driver_inv_PC(network_data, file_name);
-
+    % Start the simulation
+    [Alpha, P, A] = driver_inv_PC(network_data, file_name);  % Call your existing simulation function
 end
