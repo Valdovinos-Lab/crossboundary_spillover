573fc166e6b21d91e0682b4b628fef99894b237f
rnelson26 <ranelson526@gmail.com>
2025-01-16 15:21:36 -0800

    matlab update

--- run.m
+++ run.m
@@ -17,80 +17,92 @@
 %%%%% Updates 12/23/2023 Lincolnshire, IL by Becca Nelson:
 %%% began modification of code for cross-boundary spillover project 
 
+%%% Updates 1/16/25 Davis, CA by Becca Nelson:
+%%% created a .run code where the plant species removed get replaced with
+%%% zeros, modified the integrate file to include separate u values for
+%%% serpentine and non-serpentine but still getting error messages
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % Run a suite of simulations
 function [] = run()
-    global network
+    % List of network CSV files
+    networks = {'bertha_2022.csv'};  % Use one network as a test 
 
-    % Set directory where .csv files are located
-    network_dir = '/Users/Becca/Desktop/Harrison Lab/crossboundary_spillover';  % Directory with the .csv files
-
-    % Get a list of .csv files in the directory
-    network_files = dir(fullfile(network_dir, '*.csv'));
-
-    % Define the columns you want to work with
-    columns_to_keep = {'ASER', 'VIVI', 'CESO', 'MEIN', 'PHAQ', 'ANAR', 'AMME', 'ERCI', 'mustard', 'yellow_aster', 'dandelion', 'MEPO', 'SEVU'};
-
-    % Loop through each CSV file and process
-    for i = 1:length(network_files)
-        % Load the current network CSV file
-        network_file = network_files(i).name;
-        full_file_path = fullfile(network_dir, network_file);
+    for network_index = 1:length(networks)
+        network_file = networks{network_index};
         
-        % Read the CSV file into a table
-        data = readtable(full_file_path);
+        % Read the entire CSV as a table
+        tbl = readtable(network_file, 'ReadRowNames', true);
 
-        % Create a list of columns to keep (if they exist)
-        existing_columns = intersect(columns_to_keep, data.Properties.VariableNames);  % Check which columns exist
+        % Extract species names (row identifiers)
+        row_ids = tbl.Properties.RowNames;
 
-        % 1) Create CSV with only Non-serpentine ('columns to keep') (data_NS)
-        data_NS = data(:, existing_columns);  % Keep only the NS columns that exist
-        writetable(data_NS, fullfile(network_dir, sprintf('data_NS_%s', network_file)));
+        % Extract column species names (header row)
+        col_ids = tbl.Properties.VariableNames;
 
-        % 2) Create CSV with all columns except ASER, VIVI, CESO (if they exist) (data_S)
-        columns_to_remove = setdiff(data.Properties.VariableNames, existing_columns);  % Remove NS columns if they exist
-        data_S = data(:, columns_to_remove);  % Keep only serpentine columns
-        writetable(data_S, fullfile(network_dir, sprintf('data_S_%s', network_file)));
-
-        % 3) Run simulations on three versions of the data (original, data_NS, data_S)
+        % Extract numeric data (the core of the matrix)
+        network_data = table2array(tbl);
         
-        % a) Original full data (network_data)
-        network_data = data;  % Use the original data structure
-        run_simulation(network_data, network_file, 'Original');
+        % Define non-serpentine species rows
+        non_serp_rows = {'ASER', 'VIVI', 'CESO', 'MEIN', 'PHAQ', 'ANAR', 'AMME', ...
+                         'ERCI', 'mustard', 'yellow_aster', 'dandelion', 'MEPO', 'SEVU'};
+        
+        % Create a mask for serpentine and non-serpentine species
+        non_serp_mask = ismember(row_ids, non_serp_rows);
+        serpentine_mask = ~non_serp_mask;  % Serpentine is the opposite of non-serpentine
 
-        % b) Non-serpentine data (data_NS)
-        run_simulation(data_NS, network_file, 'Non-serpentine');
-
-        % c) Serpentine data (data_S)
-        run_simulation(data_S, network_file, 'Serpentine');
+  
+        % Create three versions of the network data
+        full_network = network_data;  % Version 1: Original
+        
+        % Version 2: Serpentine (Non-serp rows set to zero)
+        serpentine_network = network_data;
+        serpentine_network(~serpentine_mask, :) = 0;
+        
+        % Version 3: Nonserpentine only (Only non-serp rows kept, others set to zero)
+        nonserpentine_network = network_data;
+        nonserpentine_network(non_serp_mask, :) = 0;
+        
+        % List of data versions for simulation
+        data_versions = {full_network, serpentine_network, nonserpentine_network};
+        version_names = {'full_network', 'serpentine_network', 'nonserpentine_network'};
+        
+        % Run simulation for each data version
+        for version_index = 1:length(data_versions)
+            data = data_versions{version_index};
+            version_name = version_names{version_index};
+            
+            % Specify mortality case 
+            death_case = 3;
+            
+            % Construct file name
+            file_name = sprintf('%s_%s_case%d', erase(network_file, '.csv'), version_name, death_case);
+            
+            % Run simulation with appropriate u
+            if strcmp(version_name, 'serpentine_network')
+                % Run simulation for serpentine network
+                [Alpha, P, A] = run_inv_PC(file_name, data, u_serpentine);
+            elseif strcmp(version_name, 'nonserpentine_network')
+                % Run simulation for non-serpentine network
+                [Alpha, P, A] = run_inv_PC(file_name, data, u_nonserpentine);
+            else
+                % Run simulation for full network (both serpentine and non-serpentine)
+                [Alpha, P, A] = run_inv_PC(file_name, data, {u_serpentine, u_nonserpentine});
+            end
+            
+            % Save results
+            plant_data = [full(P{1}), full(P{2})];
+            animal_data = [full(A{1}), full(A{2})];
+            alpha_data = [full(Alpha{1}), full(Alpha{2})];
+            
+            % Write data to CSV files
+            writematrix(plant_data, sprintf('data/P_%s.csv', file_name));
+            writematrix(animal_data, sprintf('data/A_%s.csv', file_name));
+            writematrix(alpha_data, sprintf('data/Alpha_%s.csv', file_name));
+        end
     end
 end
 
-function [] = run_simulation(data, network_file, data_type)
-    global network
 
-    % Run the simulation on the provided data (original, data_NS, or data_S)
-    network = data;  % If needed, adjust how you use the data in your simulation
-
-    % Run for mortality situation 3 (low mortality for animals and plants)
-    death_case = 3;
-
-    % Construct a file name for the current network and death case
-    file_name = sprintf('%s_%s_m%d', network_file(1:end-4), data_type, death_case);  % Append the data type to the filename
-
-    %% Run simulation 
-    [Alpha, P, A] = run_inv_PC(data, file_name);
-
-    %% Write data to a csv file
-    plant_data = [full(P{1}), full(P{2})];
-    animal_data = [full(A{1}), full(A{2})];
-    alpha_data = [full(Alpha{1}), full(Alpha{2})];                
-
-    % Write output data to csv files
-    writematrix(plant_data, sprintf('data/P_%s.csv', file_name));
-    writematrix(animal_data, sprintf('data/A_%s.csv', file_name));
-    writematrix(alpha_data, sprintf('data/Alpha_%s.csv', file_name));
-end
-
-
+          
