3a505300a3aa3c911e432e2407473370ffe43e56
rnelson26 <ranelson526@gmail.com>
2025-03-18 17:00:54 -0700

    network properties

--- crossboundary_spillover-main/analysis.R
+++ crossboundary_spillover-main/analysis.R
@@ -11,7 +11,7 @@ library(tidyverse)
 ###### Load combined outputs ######
 animal <- read.csv("animal_outputs.csv")
 plant <- read.csv("plant_outputs.csv")
-
+network <- read.csv("networks_full.csv") #empirical networks 
 
 ## version 1 is both, version 2 is serpentine, version 3 is non-serpentine
 ## 1 is AF, 2 is no AF 
@@ -428,4 +428,353 @@ animal_long %>% filter(ARTH == "BOVO") %>% ggplot() +
   facet_wrap(~site_year)
 
 
+####### Explore networks by Plant Species  #######
+## organize network info in networks_input.R first
 
+TRFU_full <- left_join(TRFU, spring, by = "site_year") #check how rows work out
+TRFU_close <- left_join(TRFU, spring_close, by = "site_year")
+TRFU_between <- left_join(TRFU, spring_between, by = "site_year")
+TRFU_jaccard <- left_join(TRFU, spring_jaccard, by = "site_year")
+TRFU_sorensen <- left_join(TRFU, spring_sorensen, by = "site_year")
+
+TRFU_full %>% filter(metric == "VIVI_on_TRFU")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quanity, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+TRFU_full %>% filter(metric == "TRFU_on_VIVI")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quanity, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+
+TRFU_full %>% filter(metric == "Ind_Contribution_TRFU")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+TRFU_full %>% filter(metric == "Ind_Contribution_VIVI")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+TRFU_close  %>% ggplot() +
+  geom_point(mapping = aes(x = VIVI, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+TRFU_between  %>% ggplot() +
+  geom_point(mapping = aes(x = VIVI, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+TRFU_jaccard  %>% ggplot() +
+  geom_point(mapping = aes(x = Jaccard_Value, y = visit_quanity, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+TRFU_sorensen  %>% ggplot() +
+  geom_point(mapping = aes(x = Sorensen_Value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+##### Vetch
+
+VIVI_full <- left_join(VIVI, spring, by = "site_year") #check how rows work out
+VIVI_close <- left_join(VIVI, spring_close, by = "site_year")
+VIVI_between <- left_join(VIVI, spring_between, by = "site_year")
+VIVI_jaccard <- left_join(VIVI, spring_jaccard, by = "site_year")
+VIVI_sorensen <- left_join(VIVI, spring_sorensen, by = "site_year")
+
+VIVI_full %>% filter(metric == "VIVI_on_TRFU")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+VIVI_full %>% filter(metric == "TRFU_on_VIVI")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+
+VIVI_full %>% filter(metric == "Ind_Contribution_TRFU")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+VIVI_full %>% filter(metric == "Ind_Contribution_VIVI")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+VIVI_close  %>% ggplot() +
+  geom_point(mapping = aes(x = VIVI, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+VIVI_between  %>% ggplot() +
+  geom_point(mapping = aes(x = VIVI, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+VIVI_jaccard  %>% ggplot() +
+  geom_point(mapping = aes(x = Jaccard_Value, y = visit_quanity, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+VIVI_sorensen  %>% ggplot() +
+  geom_point(mapping = aes(x = Sorensen_Value, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+   
+########## LACA
+
+LACA_full <- left_join(LACA, spring, by = "site_year") #check how rows work out
+LACA_close <- left_join(LACA, spring_close, by = "site_year")
+LACA_between <- left_join(LACA, spring_between, by = "site_year")
+LACA_jaccard <- left_join(LACA, spring_jaccard, by = "site_year")
+LACA_sorensen <- left_join(LACA, spring_sorensen, by = "site_year")
+
+LACA_full %>% filter(metric == "VIVI_on_TRFU")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+LACA_full %>% filter(metric == "TRFU_on_VIVI")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quanity, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+
+LACA_full %>% filter(metric == "Ind_Contribution_TRFU")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+LACA_full %>% filter(metric == "Ind_Contribution_VIVI")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+LACA_close  %>% ggplot() +
+  geom_point(mapping = aes(x = VIVI, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+LACA_between  %>% ggplot() +
+  geom_point(mapping = aes(x = VIVI, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+LACA_jaccard  %>% ggplot() +
+  geom_point(mapping = aes(x = Jaccard_Value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+LACA_sorensen  %>% ggplot() +
+  geom_point(mapping = aes(x = Sorensen_Value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+####### HEEX
+
+HEEX_full <- left_join(HEEX, summer, by = "site_year") #check how rows work out
+HEEX_close <- left_join(HEEX, summer_close, by = "site_year")
+HEEX_between <- left_join(HEEX, summer_between, by = "site_year")
+HEEX_jaccard <- left_join(HEEX, summer_jaccard, by = "site_year")
+HEEX_sorensen <- left_join(HEEX, summer_sorensen, by = "site_year")
+
+HEEX_full %>% filter(metric == "CESO_on_HEEX")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+HEEX_full %>% filter(metric == "CESO_on_HEEX")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+
+HEEX_full %>% filter(metric == "Ind_Contribution_HEEX")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+HEEX_full %>% filter(metric == "Ind_Contribution_CESO")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+HEEX_close  %>% ggplot() +
+  geom_point(mapping = aes(x = CESO, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+HEEX_between  %>% ggplot() +
+  geom_point(mapping = aes(x = HEEX, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+HEEX_jaccard  %>% ggplot() +
+  geom_point(mapping = aes(x = Jaccard_Value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+HEEX_sorensen  %>% ggplot() +
+  geom_point(mapping = aes(x = Sorensen_Value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+######## CESO
+
+CESO_full <- left_join(CESO, summer, by = "site_year") #check how rows work out
+CESO_close <- left_join(CESO, summer_close, by = "site_year")
+CESO_between <- left_join(CESO, summer_between, by = "site_year")
+CESO_jaccard <- left_join(CESO, summer_jaccard, by = "site_year")
+CESO_sorensen <- left_join(CESO, summer_sorensen, by = "site_year")
+
+CESO_full %>% filter(metric == "CESO_on_HEEX")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+CESO_full %>% filter(metric == "HEEX_on_CESO")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+
+CESO_full %>% filter(metric == "Ind_Contribution_HEEX")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+ CESO_full %>% filter(metric == "Ind_Contribution_CESO")  %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+CESO_close  %>% ggplot() +
+  geom_point(mapping = aes(x = HEEX, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+CESO_between  %>% ggplot() +
+  geom_point(mapping = aes(x = HEEX, y = visit_quality, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+CESO_jaccard  %>% ggplot() +
+  geom_point(mapping = aes(x = Jaccard_Value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+CESO_sorensen  %>% ggplot() +
+  geom_point(mapping = aes(x = Sorensen_Value, y = reward_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version)
+
+###### Explore Networks--Network Level properties ######
+plant_long <- left_join(plant_long, network, by = "site_year")
+plant_summary <- left_join(plant_summary, network, by = "site_year")
+
+### overall plant responses 
+plant_summary %>% filter(metric == "NODF") %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = visit_quanity, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version) +  labs(x = "NODF")
+
+plant_summary %>% filter(metric == "weighted NODF") %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = plant_abundance, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version) +  labs(x = "weighted NODF")
+
+plant_summary %>% filter(metric == "niche.overlap.HL") %>% ggplot() +
+  geom_point(mapping = aes(x = raw_value, y = sum_pol, color = AF), 
+             position = position_jitter(width = 0.2, height = 0), 
+             alpha = 0.7) + 
+  theme_classic() + 
+  facet_wrap(~version) +  labs(x = "niche.overlap.HL")
--- crossboundary_spillover-main/network_inputs.R
+++ crossboundary_spillover-main/network_inputs.R
@@ -17,10 +17,13 @@ summer_between <- read.csv("network_betweenness_summer.csv")
 summer_jaccard <- read.csv("ceso_vs_heex_jaccard_values.csv")
 summer_sorensen <- read.csv("ceso_vs_heex_sorensen_values.csv")
 spring_jaccard <- read.csv("vivi_vs_trfu_jaccard_values.csv")
-spring_jaccard <- read.csv("vivi_vs_trfu_sorensen_values.csv")
+spring_sorensen <- read.csv("vivi_vs_trfu_sorensen_values.csv")
 
 spring <- read.csv("spring_network_metrics_2_4_25.csv")
 summer <- read.csv("summer_network_metrics_2_4_25.csv")
+
+spring_full <- read.csv("spring_metrics_10_17_24.csv")
+summer_full <- read.csv("summer_metrics_10_24_24.csv")
 
 sort(unique(plant_long$site_year))
 sort(unique(animal_long$site_year)) ##issue with coyote 24 and pond 22
@@ -58,3 +61,49 @@ summer_sorensen <- summer_sorensen %>%
                           "Aikawa_2022" = "Aikawa_2022_summer", 
                           "Bertha_2022" = "Bertha_2022_summer",
                           "Bertha_2023" = "Bertha_2023_summer"))
+
+summer_full <- summer_full %>%
+  mutate(treatment = recode(treatment, 
+                          "Aikawa_2022" = "Aikawa_2022_summer", 
+                          "Bertha_2022" = "Bertha_2022_summer",
+                          "Bertha_2023" = "Bertha_2023_summer"))
+
+### standardize names
+summer_sorensen <- summer_sorensen %>%
+  rename(site_year = Network)
+summer_jaccard <- summer_jaccard %>%
+  rename(site_year = Network)
+spring_sorensen <- spring_sorensen %>%
+  rename(site_year = Network)
+spring_jaccard <- spring_jaccard %>%
+  rename(site_year = Network)
+
+
+summer <- summer %>%
+  rename(site_year = site)
+summer_close <- summer_close %>%
+  rename(site_year = site)
+summer_between <- summer_between %>%
+  rename(site_year = site)
+
+spring <- spring %>%
+  rename(site_year = site)
+spring_close <- spring_close %>%
+  rename(site_year = site)
+spring_between <- spring_between %>%
+  rename(site_year = site)
+
+spring_full <- spring_full %>%
+  rename(site_year = treatment)
+
+summer_full <- summer_full %>%
+  rename(site_year = treatment)
+
+###### combine dataframes ######
+networks <- rbind(spring, summer)
+jaccard <- rbind(spring_jaccard, summer_jaccard)
+sorensen <- rbind(spring_sorensen, summer_sorensen)
+networks_full <- rbind(spring_full, summer_full)
+
+write.csv(networks_full, "networks_full.csv", row.names = FALSE)
+
